5573a77e4f1da0057e58c1edf5871a2d078e22cb
Danton Matheus <dantoncosta03@hotmail.com>
2022-06-16 16:34:26 -0300

    :white_check_mark: Cria a rota get para os tweets

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import express from "express";
 
 const app = express();
+const tweets = [];
 
-app.listen(5000, () => {console.log('🛰️  Servidor iniciado!\nAcesse: http://localhost:5000')});
+app.get("/tweets", (_,res) => {
+  if (tweets.length < 10) {
+    res.send(tweets);
+  }
+  else{
+    const ultimosTweets = tweets.slice(-10); 
+    res.send(ultimosTweets);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('🛰️  Servidor iniciado!\nAcesse: http://localhost:5000')
+});
